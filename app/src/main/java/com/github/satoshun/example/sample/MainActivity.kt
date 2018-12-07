a1dd18cb9e25ad77be4a54fcbca9af329b997ef7
SatoShun <shun.sato1@gmail.com>
2018-12-07 00:16:04 +0000

    dagger sample

--- app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.github.satoshun.example.sample
 
+import android.content.Context
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.github.satoshun.example.sample.databinding.MainActBinding
+import dagger.android.AndroidInjection
+import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
+  @Inject lateinit var resource: UserResource
+
   override fun onCreate(savedInstanceState: Bundle?) {
+    AndroidInjection.inject(this)
     super.onCreate(savedInstanceState)
     val binding = DataBindingUtil.setContentView<MainActBinding>(this, R.layout.main_act)
   }
 }
+
+@ActivityScope
+class UserResource @Inject constructor(private val context: Context)
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,60 +1,62 @@
 object Vers {
-  val compile_sdk = 28
-  val min_sdk = 25
-  val target_sdk = 28
+  const val compile_sdk = 28
+  const val min_sdk = 25
+  const val target_sdk = 28
 
-  val kotlin = "1.3.11"
-  val couroutine = "1.0.0"
+  const val kotlin = "1.3.11"
+  const val couroutine = "1.0.0"
 }
 
 object Libs {
-  val android_plugin = "com.android.tools.build:gradle:3.4.0-alpha07"
-  val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
-  val versions_plugin = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
+  const val android_plugin = "com.android.tools.build:gradle:3.4.0-alpha07"
+  const val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
+  const val versions_plugin = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
 
-  val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
-  val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vers.couroutine}"
-  val ui_coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Vers.couroutine}"
+  const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
+  const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Vers.couroutine}"
+  const val ui_coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Vers.couroutine}"
 
-  val ktx = "androidx.core:core-ktx:1.0.0"
+  const val ktx = "androidx.core:core-ktx:1.0.0"
 
-  val appcompat = "androidx.appcompat:appcompat:1.0.2"
-  val recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
-  val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
-  val cardview = "androidx.cardview:cardview:1.0.0"
+  const val appcompat = "androidx.appcompat:appcompat:1.0.2"
+  const val recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
+  const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha2"
+  const val cardview = "androidx.cardview:cardview:1.0.0"
 
-  val activityx = "androidx.activity:activity-ktx:1.0.0-alpha01"
-  val fragmentx = "androidx.fragment:fragment-ktx:1.1.0-alpha01"
+  const val activityx = "androidx.activity:activity-ktx:1.0.0-alpha01"
+  const val fragmentx = "androidx.fragment:fragment-ktx:1.1.0-alpha01"
 
-  val material = "com.google.android.material:material:1.0.0"
-  val coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
+  const val material = "com.google.android.material:material:1.0.0"
+  const val coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
 
-  val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
-  val livedata = "androidx.lifecycle:lifecycle-livedata:2.0.0"
-  val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:2.0.0"
-  val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+  const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+  const val livedata = "androidx.lifecycle:lifecycle-livedata:2.0.0"
+  const val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:2.0.0"
+  const val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:2.0.0"
 
-  private val vmoshi = "1.8.0"
-  val moshi = "com.squareup.moshi:moshi:$vmoshi"
-  val moshi_kt = "com.squareup.moshi:moshi-kotlin:$vmoshi"
-  val moshi_codegen = "com.squareup.moshi:moshi-kotlin-codegen:$vmoshi"
-  val retrofit_moshi = "com.squareup.retrofit2:converter-moshi:2.4.0"
+  private const val vmoshi = "1.8.0"
+  const val moshi = "com.squareup.moshi:moshi:$vmoshi"
+  const val moshi_kt = "com.squareup.moshi:moshi-kotlin:$vmoshi"
+  const val moshi_codegen = "com.squareup.moshi:moshi-kotlin-codegen:$vmoshi"
+  const val retrofit_moshi = "com.squareup.retrofit2:converter-moshi:2.4.0"
 
-  private val vdagger = "2.18"
-  val dagger = "com.google.dagger:dagger:$vdagger"
-  val dagger_processor = "com.google.dagger:dagger-compiler:$vdagger"
+  private const val vdagger = "2.19"
+  const val dagger = "com.google.dagger:dagger:$vdagger"
+  const val dagger_processor = "com.google.dagger:dagger-compiler:$vdagger"
+  const val dagger_android = "com.google.dagger:dagger-android:$vdagger"
+  const val dagger_android_processor = "com.google.dagger:dagger-android-processor:$vdagger"
+  const val dagger_android_support = "com.google.dagger:dagger-android-support:$vdagger"
 
-  val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
-  val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
-  val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
-  val rxbinding = "com.jakewharton.rxbinding2:rxbinding:2.2.0"
+  const val rxjava = "io.reactivex.rxjava2:rxjava:2.2.0"
+  const val rxandroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
+  const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
+  const val rxbinding = "com.jakewharton.rxbinding2:rxbinding:2.2.0"
 
-  val junit = "junit:junit:4.12"
-  val truth = "com.google.truth:truth:0.42"
-  val test_rules = "androidx.test:rules:1.1.0"
-  val test_runner = "androidx.test:runner:1.1.0"
-  val test_junit_rules = "androidx.test.ext:junit:1.0.0"
-  val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
-  val espresso = "androidx.test.espresso:espresso-core:3.1.0"
-  val robolectric = "org.robolectric:robolectric:4.0"
+  const val truth = "com.google.truth:truth:0.42"
+  const val test_rules = "androidx.test:rules:1.1.0"
+  const val test_runner = "androidx.test:runner:1.1.0"
+  const val test_junit_rules = "androidx.test.ext:junit:1.0.0"
+  const val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
+  const val espresso = "androidx.test.espresso:espresso-core:3.1.0"
+  const val robolectric = "org.robolectric:robolectric:4.0"
 }
